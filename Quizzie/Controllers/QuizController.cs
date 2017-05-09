742fa1bc813d7251f8978e61fddf78642e221382
Madeleine Ingemarsson <madeleine.ingemarsson@gmail.com>
2017-05-09 13:16:05 +0200

    Added QuizIndexVM

--- Quizzie/Controllers/QuizController.cs
+++ Quizzie/Controllers/QuizController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Quizzie.Controllers
 {
-    public class QuizController
+    public class QuizController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
     }
 }
---
+++ Quizzie/Models/VM/QuizIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quizzie.Models.VM
+{
+    public class QuizIndexVM
+    {
+        public string Question { get; set; }
+
+    }
+}
