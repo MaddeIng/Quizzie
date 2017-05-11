68e82d54585c941b7d70908fdbb2d94af1e6cba4
torselden <tor.selden@gmail.com>
2017-05-11 13:28:17 +0200

    Added SignalR and QuizHub-class. Added js to handle the click-event on a button to check if it is correct.

---
+++ Quizzie/QuizHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using Quizzie.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quizzie
+{
+    public class QuizHub : Hub
+    {
+        public void PushQuiz()
+        {
+            var quiz = Quiz.GetQuiz();
+            Clients.All.pushQuiz(quiz);
+        }
+
+        public bool IsCorrect(int quizQuestionAnswerID)
+        {
+            QuizzieDBContext context = new QuizzieDBContext();
+
+            var isCorrect = context.QuizQuestionAnswers
+                .SingleOrDefault(a => a.ID == quizQuestionAnswerID)
+                .IsCorrect;                
+
+            //Clients.Caller.isCorrect(isCorrect);
+            return isCorrect;
+        }
+
+    }
+}
---
+++ Quizzie/Scripts/Question.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    var quizHub = $.connection.quizHub;
+    
+    $.connection.hub.start()
+        .done(function () {
+            console.log("Hub started!");
+
+            //Hanterar klick på svarsknappar och hämtar svar (true/false) från databas 
+            $("input").click(function (event) {
+                quizHub.server.isCorrect(event.target.id)
+                    .done(function (result) {  
+                        var buttonClass = "";
+                        if (result === true) {
+                            buttonClass = "btn btn-success";
+                        }
+                        else {
+                            buttonClass = "btn btn-danger";
+                        }
+                        //console.log(result + ' ' + event.target.id + ' ' + buttonClass + ' ' + event.target);
+                        //$(event.target).flip();
+                        $(event.target).removeClass("btn btn-info").addClass(buttonClass);
+                        $("input").off("click");
+                    })
+                    .fail();
+            });
+
+        })
+        .fail(function () { alert("Fail!"); });
+
+
+})();
