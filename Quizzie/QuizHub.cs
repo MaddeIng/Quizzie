26633d2006182cf9b6979136f0e87e9ca066acb9
torselden <tor.selden@gmail.com>
2017-05-12 10:33:36 +0200

    Merge with awesome solution to VM, view and js

--- Quizzie/QuizHub.cs
+++ Quizzie/QuizHub.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
 using Quizzie.Models.Entities;
+using Quizzie.Models.VM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +11,32 @@ namespace Quizzie
 {
     public class QuizHub : Hub
     {
-        public void PushQuiz()
+        public void Initialize()
+        {
+            //Find the question to start with
+            var questionId = 2; //Chosen by fair dice roll
+
+            //Get the question
+            var question = QuizQuestion.GetQuestionViewModel(questionId);
+
+            SetQuestion(Clients.Caller, question);
+
+        }
+
+        public string GetQuiz()
         {
             var quiz = Quiz.GetQuiz();
-            Clients.All.pushQuiz(quiz);
+
+            string title = quiz.Title;
+
+            var questions = QuizQuestion.GetQuestionViewModel(2);
+
+            questions.QuizTitle = title;
+
+            var json = JsonConvert.SerializeObject(questions);
+            return json;
         }
+
 
         public bool IsCorrect(int quizQuestionAnswerID)
         {
@@ -21,10 +44,31 @@ namespace Quizzie
 
             var isCorrect = context.QuizQuestionAnswers
                 .SingleOrDefault(a => a.ID == quizQuestionAnswerID)
-                .IsCorrect;                
+                .IsCorrect;
 
-            //Clients.Caller.isCorrect(isCorrect);
+            DelayedChangeQuestion(Clients.Caller, QuizQuestion.GetQuestionViewModel(3));
+
             return isCorrect;
+        }
+
+
+        private void SetQuestion(dynamic target, QuizQuestionVM question)
+        {
+            // Make the answers serializable
+            var answers = question.Answers.Select(a => new { Answer = a.Answer, ID = a.ID }).ToList();
+
+            // Send the answers to the caller of this function
+            Clients.Caller.setQuestion(question.Question.Question, answers);
+        }
+
+        private void DelayedChangeQuestion(dynamic target, QuizQuestionVM question)
+        {
+            System.Threading.Timer timer = null;
+            timer = new System.Threading.Timer((obj) =>
+            {
+                SetQuestion(target, question);
+                timer.Dispose();
+            }, null, 1000, System.Threading.Timeout.Infinite);
         }
 
     }
--- Quizzie/Scripts/Question.js
+++ Quizzie/Scripts/Question.js
@@ -1,28 +1,59 @@
 ﻿(function () {
     var quizHub = $.connection.quizHub;
-    
+
+    function handleInputClick(event) {
+        quizHub.server.isCorrect($(this).data().answer)
+            .done(function (result) {
+
+                var buttonClass = "";
+                if (result === true) {
+                    buttonClass = "btn btn-success";
+                }
+                else {
+                    buttonClass = "btn btn-danger";
+                }
+                $(event.target).removeClass("btn btn-info").addClass(buttonClass);
+                $("input").off("click");
+            })
+            .fail(function (event) {
+                console.log("FAAAIL");
+                console.log(arguments);
+            });
+    }
+
+    quizHub.client.setQuestion = function (question, answers) {
+        var $loading = $("#loading")
+        var $currentQuestion = $("#currentQuestion")
+        var $question = $("#question")
+        var $answers = $("#answers")
+
+        $question.text(question);
+        $answers.empty();
+        for (var i = 0; i < answers.length; i++) {
+            (function (index) {
+                var btn = $("<input/>", {
+                    type: 'button',
+                    class: 'btn btn-info',
+                    value: answers[index].Answer,
+                    click: handleInputClick,
+                    "data-answer": answers[index].ID
+                });
+                $answers.append(btn);
+            })(i);
+        }
+
+        $loading.hide();
+        $currentQuestion.show();
+    }
+
     $.connection.hub.start()
         .done(function () {
             console.log("Hub started!");
+            // Tell the server to initialize us.
+            quizHub.server.initialize();
 
             //Hanterar klick på svarsknappar och hämtar svar (true/false) från databas 
-            $("input").click(function (event) {
-                quizHub.server.isCorrect(event.target.id)
-                    .done(function (result) {  
-                        var buttonClass = "";
-                        if (result === true) {
-                            buttonClass = "btn btn-success";
-                        }
-                        else {
-                            buttonClass = "btn btn-danger";
-                        }
-                        //console.log(result + ' ' + event.target.id + ' ' + buttonClass + ' ' + event.target);
-                        //$(event.target).flip();
-                        $(event.target).removeClass("btn btn-info").addClass(buttonClass);
-                        $("input").off("click");
-                    })
-                    .fail();
-            });
+            $("input").click();
 
         })
         .fail(function () { alert("Fail!"); });
