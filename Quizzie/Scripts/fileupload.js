3f9fc2d90f6234983269ca803a29afbe92ac6de7
torselden <tor.selden@gmail.com>
2017-05-17 20:16:27 +0200

    Changes!

--- Quizzie/Scripts/fileupload.js
+++ Quizzie/Scripts/fileupload.js
@@ -1,39 +1,52 @@
 ﻿function getFile() {
     document.getElementById("upfile").click();
+    console.log("Getfile");
 }
 
-function sub(obj) {
-    var file = obj.value;
-    var fileName = file.split("\\");
-    document.getElementById("bild").innerHTML = fileName[fileName.length - 1];
-    document.myForm.submit();
-    event.preventDefault();
-}
 
-//$(document).ready(function () {
+$("#trashCan").click(function () {
+    $("#targetImg").val('');
+    $("#targetImg").attr("src", "");
+    $("#imgPreview").hide();
+    console.log("hider");
+});
 
-//    $("#upfile").change(function () {
-//        var File = this.files;
-
-//        if (File && File[0]) {
-//            ReadImage(File[0]);
-//        }
-//    });
-//});
-
-//var ReadImage = function (file) {
-
-//    var reader = new FileReader;
-//    var image = new Image;
-
-//    reader.readAsDataURL(file);
-//    reader.onload(function (_file) {
-//        image.src = _file.target.result;
-//        image.onload = function () {
-
-
-//            $("#targetImg").attr("src", _file.target.result);
-//        };
-//    });
-
+//function sub(obj) {
+//    var file = obj.value;
+//    var fileName = file.split("\\");
+//    document.getElementById("bild").innerHTML = fileName[fileName.length - 1];
+//    //document.myForm.submit();
+//    //event.preventDefault();
 //}
+
+
+$(document).ready(function () {
+
+
+    $("#upfile").change(function () {
+        var File = this.files;
+
+        if (File && File[0]) {
+            ReadImage(File[0]);
+        }
+    });
+});
+
+var ReadImage = function (file) {
+
+    var reader = new FileReader();
+    var image = new Image;
+
+    reader.readAsDataURL(file);
+    reader.onload = function (_file) {
+        image.src = _file.target.result;
+        var targetImg = $("#targetImg");
+
+
+        image.onload = function () {
+            console.log("upfile");
+            targetImg.attr("src", image.src);
+            $("#imgPreview").show();
+        };
+    }
+}
