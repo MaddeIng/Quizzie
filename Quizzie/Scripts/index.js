e6ad9af651be517b12d6aa39f31e6ea8d4c358f4
torselden <tor.selden@gmail.com>
2017-05-18 10:39:34 +0200

    remoce spinner

--- Quizzie/Scripts/index.js
+++ Quizzie/Scripts/index.js
@@ -4,7 +4,7 @@
         url: "/Quiz/GetPartialViewIndex", type: "GET",
         success: function (result) {
             $("#main-body").html(result);
-            $.loader.open();
+            //$.loader.open();
             SetIndexPage();
         }
     });
@@ -12,7 +12,7 @@
     var quizHub = $.connection.quizHub;
 
     function SetIndexPage() {
-        $.loader.close();
+        //$.loader.close();
         $("#start-div #start-btn").click(function () {
             var playerName = $("#player-name").val();
             var accessCode = $("#access-code").val();
@@ -60,7 +60,7 @@
 
     function handleInputClick(event) {
 
-        $.loader.open({ title: "Väntar på samtliga svar" }); //TS
+        //$.loader.open({ title: "Väntar på samtliga svar" }); //TS
 
         quizHub.server.isCorrect($(this).data().answer)
             .done(function (result) {
@@ -86,7 +86,7 @@
 
     quizHub.client.setQuestion = function (question, answers) {
 
-        $.loader.close(); //TS
+        //$.loader.close(); //TS
 
         var $currentQuestion = $("#currentQuestion");
         var $question = $("#question");
@@ -111,7 +111,7 @@
     };
 
     quizHub.client.calculateFinalScore = function () {
-        $.loader.close();
+        //$.loader.close();
         $.ajax({
             url: "/Quiz/GetPartialViewResults", type: "GET",
             success: function (result) {
